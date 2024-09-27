8895b12615b4649a216c9c789935b1449a245640
ActuallyPanda56 <nietoc0595@gmail.com>
2024-09-27 15:33:02 -0500

    <feat> Country Overview and details

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "upload.wikimedia.org",
+        port: "", // Optional, leave empty for default
+        pathname: "/**", // Adjust this as necessary
+      },
+    ],
+  },
+};
 
 export default nextConfig;
